d87327cd7b67967dbe6cc7b3829da2a8a396899f
liwenhui <lwhlqm520>
2019-02-19 16:34:05 +0800

    版本冲突

--- src/main/java/com/dodou/liwh/starterpom/HelloStarter.java
+++ src/main/java/com/dodou/liwh/starterpom/HelloStarter.java
@@ -1,3 +1,4 @@
+/*
 package com.dodou.liwh.starterpom;
 
 import com.starter.liwh.service.HelloService;
@@ -5,13 +6,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+*/
 /**
  * @author: Liwh
  * @ClassName: HelloStarter
  * @Description:
  * @version: 1.0.0
  * @date: 2018-11-19 3:03 PM
- */
+ *//*
+
 @RestController
 public class HelloStarter {
     @Autowired
@@ -22,3 +25,4 @@ public class HelloStarter {
         return helloService.getMsg();
     }
 }
+*/
